c3e2d28a2f5f2c4651de0c244bb6dfdebdc33fa1
Admin <admin@Admins-MacBook.local>
2021-02-16 17:23:26 -0800

    progress tracker component

--- force-app/main/default/lwc/progressTracker/progressTracker.js
+++ force-app/main/default/lwc/progressTracker/progressTracker.js
@@ -1,4 +1,66 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import createCategory from '@salesforce/apex/datatableController.createCategory';
+import { refreshApex } from '@salesforce/apex';
+import { getRecordNotifyChange } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class ProgressTracker extends LightningElement {
+export default class ModalPopupLWC extends LightningElement {
+
+    renderedCallback() {
+        if (this.isTrackerOpen) {
+            this.template.querySelector('c-datatable').refreshCategories();
+        }
+    }
+
+    isTrackerOpen = true;
+    isFormOpen = false;
+
+    openTracker() {
+        this.isTrackerOpen = true;
+    }
+    closeTracker() {
+        this.isTrackerOpen = false;
+    }
+
+    openForm() {
+        this.isFormOpen = true;
+        this.isTrackerOpen = false;
+    }
+    closeForm() {
+        this.isFormOpen = false;
+        this.isTrackerOpen = true;
+    }
+
+    nameInput = '';
+    goalInput = '';
+
+    changeNameInput(event) {
+        this.nameInput = event.target.value;
+    }
+    changeGoalInput(event) {
+        this.goalInput = event.target.value;
+    }
+
+    async addCategory() {
+        await createCategory({name: this.nameInput, goal: this.goalInput})
+        .then(result => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Category was added',
+                    variant: 'success'
+                })
+            );
+            this.closeForm();
+                
+       }).catch(error => {
+           this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error adding new category',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
+        });
+    }
 }
