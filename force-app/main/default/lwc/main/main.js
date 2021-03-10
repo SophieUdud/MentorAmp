1f0f8ea41e0f5ec61df9cf3bbff3497349a8d9e1
Admin <admin@Admins-MacBook.local>
2021-03-09 22:09:35 -0800

    initial views

--- force-app/main/default/lwc/main/main.js
+++ force-app/main/default/lwc/main/main.js
@@ -1,4 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getMentorAmpUser from '@salesforce/apex/mainComponentController.getMentorAmpUser';
 
 export default class Main extends LightningElement {
+
+    notSignedUp = false;
+    onlyMentee = false;
+    onlyMentor = false;
+    both = false;
+
+    showMenteeView = false;
+    showMentorView = false;
+    
+    @wire(getMentorAmpUser, {})
+    mtAmpUsers ({ error, data }) {
+        if (data == 'notSignedUp') {
+            this.notSignedUp = true;
+        } else if (data == 'onlyMentee') {
+            this.onlyMentee = true;
+        } else if (data == 'onlyMentor') {
+            this.onlyMentor = true;
+        } else if (data == 'both'){
+            this.both = true;
+            this.showMenteeView = true;
+        }
+    }
+
+    get options() {
+        return [
+            { label: 'Mentee View', value: 'mentee' },
+            { label: 'Mentor View', value: 'mentor' },
+        ];
+    }
+
+    value = 'mentee';
+
+    changeView (event) {
+        let view = event.detail.value;
+        console.log('view: ' + view);
+        if (view === 'mentee') {
+            this.showMenteeView = true;
+            this.showMentorView = false;
+        } else if (view === 'mentor') {
+            this.showMenteeView = false;
+            this.showMentorView = true;
+        }
+    }
+
 }
