388230624fca15c60eed9d1ea78babdd3dd9d16b
Admin <admin@Admins-MacBook.local>
2021-04-11 16:23:55 -0700

    deleted namespace references

--- force-app/main/default/lwc/datatable/datatable.js
+++ force-app/main/default/lwc/datatable/datatable.js
@@ -11,15 +11,15 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const COLS = [
     {label: '', type: 'button-icon', fixedWidth: 50, typeAttributes: { name: 'deleteCategory', iconName: 'utility:delete', variant: 'container', title: 'Delete Category', alternativeText: 'Delete Category', class:'deleteIcon'}},
     { label: 'Category', fieldName: 'Name', editable: true, hideDefaultActions: true, wrapText: true },
-    { label: 'Weekly Goal', fieldName: 'mtAmp__Weekly_Goal__c', editable: true, hideDefaultActions: true, wrapText: true },
-    { label: 'Week 1', fieldName: 'mtAmp__Week_1__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 2', fieldName: 'mtAmp__Week_2__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 3', fieldName: 'mtAmp__Week_3__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 4', fieldName: 'mtAmp__Week_4__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 5', fieldName: 'mtAmp__Week_5__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 6', fieldName: 'mtAmp__Week_6__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 7', fieldName: 'mtAmp__Week_7__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
-    { label: 'Week 8', fieldName: 'mtAmp__Week_8__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } }
+    { label: 'Weekly Goal', fieldName: 'Weekly_Goal__c', editable: true, hideDefaultActions: true, wrapText: true },
+    { label: 'Week 1', fieldName: 'Week_1__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 2', fieldName: 'Week_2__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 3', fieldName: 'Week_3__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 4', fieldName: 'Week_4__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 5', fieldName: 'Week_5__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 6', fieldName: 'Week_6__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 7', fieldName: 'Week_7__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } },
+    { label: 'Week 8', fieldName: 'Week_8__c', type: 'boolean', editable: true, hideDefaultActions: true, initialWidth: 80, cellAttributes: { alignment: 'center' } }
 ];
 
 const actions = [
